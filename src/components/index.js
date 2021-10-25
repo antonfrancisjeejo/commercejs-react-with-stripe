89e6bdcf4be5c7b433eefac40b1ecda7c97eeff6
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-10-25 18:42:10 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar/Navbar";
+import Product from "./Product/Product";
+import CartItem from "./CartItem/CartItem";
+import AddressForm from "./AddressForm/AddressForm";
+import PaymentForm from "./PaymentForm/PaymentForm";
+
+export { Navbar, Product, CartItem, AddressForm, PaymentForm };
